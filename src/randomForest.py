52eb78242ddfaf87309f8bb86abc7e5a8919c0c0
sscapco <salil.seeruttun@capco.com>
2023-01-06 09:46:25 +0000

    randomForest script with basic functions added

---
+++ src/randomForest.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+
+def model(n_estimators=10,criterion='entropy'):
+    rf=RandomForestClassifier(n_estimators, criterion)
+    return rf
+
+def train(model,X_train,y_train):
+    model.fit(X_train,y_train)
+    return(model)
+
+def predict(model,X_test):
+    y_pred=model.predict(X_test)
+    return(y_pred)
+        
