ab7c6735d15f36111810da5be5af84bc3291fcf6
sscapco <salil.seeruttun@capco.com>
2023-01-08 12:51:34 +0000

    added specific train function for rf

--- src/train.py
+++ src/train.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_curve
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import dataPreprocessing as dpp
+import randomForest as rf
+import neuralNet as nn
+import activeLearning as aL
 
 def splitXY(data):
     X=data.iloc[:,:-1].values
@@ -22,9 +22,10 @@ def evaluate(y_pred,y_test):
     print("",'\n')
     print(classification_report(y_test,y_pred))
 
-#data = dpp.output_final()
-#X_train,X_test,y_train,y_test = splitXY(data)
-#rf = model()
-#y_pred = train(rf)
-#evaluation(y_test,y_pred)
+def train_rf(X_train,y_train):
+    model = rf.model()
+    model_trained = rf.train(model,X_train,y_train)
+    return model_trained
+
+
 
